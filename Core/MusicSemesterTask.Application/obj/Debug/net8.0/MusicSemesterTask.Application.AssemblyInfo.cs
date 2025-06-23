8a95eccd3219c7fc871ee4aca75f38560afd58af
Pr0Ductor <denisiyus@gmail.com>
2025-06-23 14:37:35 +0300

    вёрстка

--- Core/MusicSemesterTask.Application/obj/Debug/net8.0/MusicSemesterTask.Application.AssemblyInfo.cs
+++ Core/MusicSemesterTask.Application/obj/Debug/net8.0/MusicSemesterTask.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicSemesterTask.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44120e786628889863bc851ca258d557b9894e8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9df0ff1b7477d2d6ff9555cfc34a2a0feac40f32")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicSemesterTask.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicSemesterTask.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MusicSemesterTask.Web/Controllers/ArtistProfileController.cs
+++ MusicSemesterTask.Web/Controllers/ArtistProfileController.cs
@@ -15,62 +15,6 @@ public class ArtistProfileController : Controller
     [HttpGet]
     public async Task<IActionResult> Index(int? id = null)
     {
-        try
-        {
-            if (!id.HasValue)
-            {
-                // Показываем профиль текущего пользователя
-                var user = await _authService.GetCurrentUserAsync();
-                if (user == null)
-                    return RedirectToAction("Login", "AuthView");
-
-                var songs = new List<GetUserSongsQueryResult>();
-                var isArtist = user.Role == UserRole.Artist;
-                
-                if (isArtist)
-                {
-                    var artist1 = await _context.Artists.FirstOrDefaultAsync(a => a.CreatedBy == user.Id);
-                    songs = await _mediator.Send(new GetUserSongsQuery { UserId = artist1.Id.ToString() });
-                }
-
-                // Получаем лайкнутые песни
-                var likedSongs = await _mediator.Send(new GetUserLikedSongsQuery { UserId = user.Id.ToString() });
-                
-                return View(new ProfileViewModel
-                {
-                    Songs = songs,
-                    ProfilePictureUrl = user.ProfilePictureUrl ?? "../assets/img/question.png",
-                    UserName = user.UserName,
-                    SongsCount = songs.Count,
-                    IsCurrentUser = true,
-                    IsArtist = isArtist,
-                    LikedSongs = likedSongs
-                });
-            }
-            
-            // Показываем профиль артиста
-            var artist = await _mediator.Send(new GetArtistByIdQuery { Id = id.Value.ToString() });
-            if (artist == null)
-                return NotFound();
-
-            var artistSongs = await _mediator.Send(new GetUserSongsQuery { UserId = id.Value.ToString() });
-            var currentUser = await _authService.GetCurrentUserAsync();
-            
-            return View(new ProfileViewModel
-            {
-                Songs = artistSongs,
-                ProfilePictureUrl = artist.ProfilePictureUrl ?? "../assets/img/question.png",
-                UserName = artist.UserName,
-                SongsCount = artistSongs.Count,
-                IsCurrentUser = currentUser?.Id == id.Value,
-                IsArtist = true,
-                LikedSongs = new List<Song>() // Empty list for artist profiles
-            });
-        }
-        catch (Exception ex)
-        {
-            // Логирование ошибки
-            return RedirectToAction("Error", "Home");
-        }
+        return View();
     }
 }
